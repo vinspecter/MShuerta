0e4b854a1790ca87dc3fbc05c3573722bf0260e5
“Vin” <vinspecter@gmail.com>
2020-04-28 20:01:10 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -3,10 +3,20 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser')
 
 // inicializar variables
 var app = express();
+
+// Body parser 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importacion de rutas 
+var appRoutes = require('./routes/app')
+var usuarioRoutes = require('./routes/usuario')
+var loginRoutes = require('./routes/login')
 
 // conexion a base de dastos 
 mongoose.connection.openUri('mongodb://localhost:27017/huertaDB', (err, res) => {
@@ -19,13 +29,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/huertaDB', (err, res) => 
 
 
 // Rutas 
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-})
+app.use('/usuario', usuarioRoutes)
+app.use('/login', loginRoutes)
+app.use('/', appRoutes)
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,29 @@
+// var express = require('express');
+// var bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
+
+var SEED = require('../config/config').SEED;
+
+
+// ============================
+// Verificar token
+// ============================
+exports.verificaToken = function(req, res, nexr) {
+
+    var token = req.query.token;
+
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'Token Incorrecto',
+                errors: err
+            })
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+
+    })
+
+}
